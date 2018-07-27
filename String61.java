43d1f226fd5a44d8380fb2b0a93d8756c67cfc29
kaviya27 <32125106+kaviya27@users.noreply.github.com>
2018-07-27 05:19:24 -0700

    Add files via upload

---
+++ String61.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class String61 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		String str = s.next();
+		int a = s.nextInt();
+		System.out.println(str.substring(0, a));
+	}
+
+}
